81b241bdf0f782a25c85f27bff40cd39772afb61
Jenny Zhu <jenny.a.zhu@gmail.com>
2025-06-24 18:36:49 -0400

    broken, stash changes

--- hack/update-fbc.sh
+++ hack/update-fbc.sh
@@ -3,8 +3,14 @@
 # for f in release-payloads/release-1.2.0/release-stage-fbc-v4-*; do bash hack/update-fbc.sh $f; done
 
 pipeline="$(basename "$1")"
-fbc="${pipeline:14:(-5)}"
-latest_snap=$(kubectl get snapshots -l appstudio.openshift.io/component=coo-"$fbc" --sort-by='.metadata.creationTimestamp' --no-headers -o custom-columns=":metadata.name" | tail -n 3 | tac | xargs kubectl get snapshots -o json | jq -r ".items[] | select(.metadata.labels.\"appstudio.openshift.io/build-pipelinerun\" | startswith(\"coo-$fbc-on-push\")).metadata.name" | head -n1)
-sed -i "s/\(snapshot: \).*/\1$latest_snap/" "$1"
+fbc=$(echo "$pipeline" | sed -E 's/^release-stage-fbc-(.*)\.ya?ml$/\1/')
+latest_snap=$(kubectl get snapshots -l appstudio.openshift.io/component=coo-"$fbc" \
+  --sort-by='.metadata.creationTimestamp' --no-headers -o custom-columns=":metadata.name" \
+  | tail -n 3 \
+  | awk '{ lines[NR] = $0 } END { for (i = NR; i > 0; i--) print lines[i] }' \
+  | xargs kubectl get snapshots -o json \
+  | jq -r ".items[] | select(.metadata.labels.\"appstudio.openshift.io/build-pipelinerun\" | startswith(\"coo-$fbc-on-push\")).metadata.name" \
+  | head -n1)
+sed -i "" "s/\(snapshot: \).*/\1$latest_snap/" "$1"
 now=$(date +%Y-%m-%d-%H-%M)
-sed -i "s/\(name: \).*/\1$latest_snap-$now/" "$1"
+sed -i "" "s/\(name: \).*/\1$latest_snap-$now/" "$1"
